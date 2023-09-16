800feab956c1560b0556f06a7936dcbd15b18d04
hiruma48 <49358000+hiruma48@users.noreply.github.com>
2023-09-16 08:36:12 +0700

    tugas 4 php
    mengerjakan tugas

---
+++ tugas/tugas4_1.php
@@ -0,0 +1,13 @@
+<?php
+$beli = 8000;
+$jual = 7500;
+$keuntungan=300;
+
+$ekor = $beli/$keuntungan;
+
+echo"Harga Beli = Rp $beli<br>" ;
+echo"Harga Jual = Rp $jual<br>";
+echo"keuntungan = Rp $keuntungan<br>";
+echo"berapa jumlah kambing yang dibeli? jawab : $ekor ekor";
+
+?>
---
+++ tugas/tugas4_2.php
@@ -0,0 +1,12 @@
+<?php
+$saluran = 523;
+$terpakai =8891;
+
+$pemakaian = $terpakai/$saluran;
+
+echo"jumlah saluran = $saluran<br>";
+echo"jumlah terpakai = $terpakai<br>";
+echo"rata-rata pemakaian  = $pemakaian <br>";
+echo"jawaban = $pemakaian liter/keluarga <br>";
+
+?>
